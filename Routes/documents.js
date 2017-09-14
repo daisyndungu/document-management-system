baaafd07f3e62c972e4c47f6d896e5c374f6542a
Daisy Ndungu <daisywndungu@gmail.com>
2017-09-14 15:41:29 +0300

    feat(document): add document model
    - adds api end point to add, delete, update and fetch a single document
    - adds api end point to fetch all the documents from db
    
    [#151036191]
    
    refactor code

--- Routes/documents.js
+++ Routes/documents.js
@@ -3,7 +3,7 @@ var express = require('express');
 var routes = function(Document){
     var documentRouter = express.Router();
     
-    // register a user
+    // add a new document
     documentRouter.route('')
         .post(function(req, res){
             if (!req.body.access){
@@ -40,7 +40,65 @@ var routes = function(Document){
                     res.status(200).send(documents);
             });
         });
+    // add a middleware to find a document by id
+    documentRouter.use('/:id', function(req, res, next){
+        Document.findById(req.params.id, function(err, document){
+            if (err) {
+                res.status(400).send(err);
+            }
+            else if (document){
+                req.document = document;
+                next();
+            }
+            else {
+                res.status(404).send('Document not found');
+            }
+        })
+    });
+    // enable delete, get and update Document details
+    documentRouter.route('/:id')
+        .get(function(req, res){
+            res.json(req.document);
+        })
+        .put(function(req, res){
+            access = [];
+            if (req.body.access){
 
+                for (var len=0; len<req.body.access.length; len++){
+                    access.push(req.body.access[len].toUpperCase());
+                }
+            }
+
+            if (access.length > 1 && access.includes('PUBLIC') ||
+            access.length > 1 && access.includes('PRIVATE')){
+                res.status(400).send('Acces rights should be "Private", "Public" or role(s) eg "Software Dev"');
+            }
+            else {
+                req.document.name = req.body.name;
+                req.document.publish_date = req.body.publish_date;
+                req.document.access = req.body.access;
+                req.document.save(function(err, document){
+                    if (err){
+                        res.send('error')
+                    }
+                    else {
+                        res.status(200).send('Document details updated succesfully');
+                    }
+                });
+            }
+            
+        })
+        .delete(function(req, res){
+            req.document.remove(function(err, success){
+                if (err) {
+                    res.status(400).send(err);
+                }
+                else {
+                    res.status(200).send('Deleted successfully');
+                }
+            });
+
+        });
     return documentRouter;
 };
 
